691d534a8863845e69bde32f35d5fa8aa6b93acf
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-04-06 13:31:04 +0300

    3-й коммит, аутентификация пользователей

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from account import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # предыдущий url для авторизации
+    # path('login/', views.user_login, name='login'),
+    # url-адреса для входа и выхода из учётной записи
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
