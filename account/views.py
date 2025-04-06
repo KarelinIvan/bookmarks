4b2feb9ee10ada2b7fe02db51ce41d175b52e3f1
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-04-06 14:46:16 +0300

    4-й коммит, аутентификация пользователей

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -23,3 +24,10 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    """ Проверяет аутентификацию текущего пользователя. Если пользователь аутентифицирован, то он исполняет
+    декорированное представление; если нет, то перенаправляет на url входа в учётную запись """
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
