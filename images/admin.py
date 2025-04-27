ab8ffb2ccdca31009bded404d2dd0c3e2f1cbbd0
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-04-27 17:19:56 +0300

    22-й коммит, админка

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from images import models
+
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'image', 'created',)
+    list_filter = ('created',)
